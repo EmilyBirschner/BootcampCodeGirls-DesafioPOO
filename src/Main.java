de33778641847777f47458b7f9667bfaaa2cd1c3
EmilyBirschner <emilyb.oliveira@hotmail.com>
2022-07-18 17:58:59 -0300

    Conclusão

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import dio.desafio.dominio.Conteudo;
-import dio.desafio.dominio.Curso;
-import dio.desafio.dominio.Mentoria;
+import dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -22,10 +20,34 @@ public class Main {
         mentoria.setDescricao("descrição mentoria");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("-");
+        devCamila.progredir();
+        System.out.println("Conteúdos concluídos Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("Conteúdos inscritos e não concluídos Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
+
+        System.out.println("----------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Joao: " + devJoao.getConteudosInscritos());
+        System.out.println("-");
+        devJoao.progredir();
+        System.out.println("Conteúdos concluídos Joao: " + devJoao.getConteudosConcluidos());
+        System.out.println("Conteúdos inscritos e não concluídos Joao: " + devCamila.getConteudosInscritos());
+        System.out.println("XP: " + devJoao.calcularTotalXp());
 
     }
 }

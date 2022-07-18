215ad65765f9dac61d97977b4a13bfda9660e81f
EmilyBirschner <emilyb.oliveira@hotmail.com>
2022-07-18 16:00:45 -0300

    Criei as classes Mentoria e Classe

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import dio.desafio.dominio.Curso;
+import dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição do curso");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição do curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+
+
+
+    }
+}
